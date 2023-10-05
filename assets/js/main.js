775672b35dbda6fe89310f0d470834ce4e1847e9
tiago <plus.tiago@gmail.com>
2023-10-05 00:35:23 +0000

    Adicionado botão Preview aos boxes de pokemons com efeito hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const previewButton = document.getElementsByClassName('preview')
 
 const maxRecords = 151
 const limit = 10
@@ -19,15 +20,38 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <button class="preview" type="button">
+                Preview
+            </button>
         </li>
     `
 }
 
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    pokeApi.getPokemons(offset, limit)
+    .then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
     })
+    .then(() => 
+    {
+        registerHoverEvent(previewButton)
+    })
+}
+
+
+function registerHoverEvent(buttons)
+{
+    for(let i = 0;i<buttons.length;i++)
+    {
+        console.log('ok');
+        buttons[i].addEventListener('mouseover',
+        () => 
+        {
+            console.log('hover 2!')
+        }
+        );
+    }
 }
 
 loadPokemonItens(offset, limit)
@@ -45,3 +69,4 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,6 +2,7 @@
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
+    console.log('Criando instancia do pokemon...');
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
@@ -19,17 +20,59 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
+        .then(function(response) {console.log('Obtendo detalhes do pokemon'); return  response.json()})
         .then(convertPokeApiDetailToPokemon)
+}
+
+function lidar(aObj)
+{
+    console.log('first call');
+    return aObj.json()
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        //.then((response) =>  response.json())
+        .then(
+            (response) =>
+            {
+                    console.log('Faendo chamado à API...');
+                    console.log('call 1'); 
+                    
+                    let resultado = response.json();
+                    console.log(resultado);
+                    return resultado
+
+
+            })
+        .then(
+            (jsonBody) =>
+            {
+                console.log('call 2'); 
+                return jsonBody.results
+            })
+        .then(
+            (pokemons)  =>
+            {
+                console.log('call 3');
+                return  pokemons.map(pokeApi.getPokemonDetail)
+            })
+        .then(
+            (detailRequests)  =>
+            {
+                console.log('call 4');
+                return Promise.all(detailRequests)
+            })
+        .then(
+            (pokemonsDetails)  =>
+            {
+                console.log('call 5');
+                return  pokemonsDetails
+            })
+        //.then((jsonBody) => jsonBody.results)
+        //.then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        //.then((detailRequests) => Promise.all(detailRequests))
+        //.then((pokemonsDetails) => pokemonsDetails)
 }
